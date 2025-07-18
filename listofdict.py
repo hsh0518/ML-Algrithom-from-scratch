51001e8896658008303ab3e37b17f44be163ae18
hsh0518 <38287162+hsh0518@users.noreply.github.com>
2025-07-18 13:56:00 -0700

    Create listofdict.py

---
+++ listofdict.py
@@ -0,0 +1,22 @@
+trainingdata = [
+  {'user_id': '123', 'age': 25, 'gender': 'F'},
+  {'user_id': '234', 'age': 30, 'gender': 'M'},
+  ...
+]
+schema = [
+  {"name": "age"}, {"name": "gender"}
+]
+返回一个 list of lists，每一行：
+[user_id, feature1, feature2, ..., label]
+
+def dataprocessing(trainingdata, schema, labels):
+    output = []
+    feature_keys = [f['name'] for f in schema]
+
+    for row in trainingdata:
+        user_id = row['user_id']
+        features = [row.get(key, None) for key in feature_keys]
+        label = labels.get(user_id, None)
+        output.append([user_id] + features + [label])
+
+    return output
